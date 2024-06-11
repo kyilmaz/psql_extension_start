17270258d5e4ed1f500d2ea588b9804dbd8ec6f3
alphaneker <alphaneker40@gmail.com>
2024-06-11 08:37:01 -0400

    DB is able to see the functions and successfully create the extension.

--- SQL/helloworld--1.0.sql
+++
@@ -1,4 +0,0 @@
--- Extension script to define a PostgreSQL function that calls the C function
-CREATE OR REPLACE FUNCTION hello_world()
-RETURNS text AS 'MODULE_PATHNAME', 'hello_world'
-LANGUAGE C STRICT;
